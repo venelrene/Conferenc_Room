aafb72d26961e2f38fd54570dcbfac43f2abef2c
Mr.Rene <venelrene@gmail.com>
2015-04-07 16:12:10 -0400

    Jason help me remove an array inside a array
    :feature_ids => [])

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -78,6 +78,8 @@ class RoomsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def room_params
-      params.require(:room).permit(:name, :campus, :room_id, :user_id, :location, :photo, :description, :squarefootage, :lastused, :roomphoto, :roomphoto_cache,  :remote_roomphoto_url, :remote_image_url, :campus_id, :feature_ids)
+      params.require(:room).permit(:name, :campus, :room_id, :user_id, :location, :photo, :description,
+                                   :squarefootage, :lastused, :roomphoto, :roomphoto_cache,  :remote_roomphoto_url,
+                                   :remote_image_url, :campus_id, :feature_ids => [])
     end
 end
